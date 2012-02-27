3165b13ea8b8879406641b247b2871e227a7e708
Fabian Bornhofen <fbornhofen@googlemail.com>
2012-02-27 13:52:26 -0800

    add attr, css wrapper functions

--- src/jquery.jrichteqst.js
+++ src/jquery.jrichteqst.js
@@ -3,6 +3,23 @@
   $.fn.jRichTeQst = function(node) {
     var widget = {
 
+      // DOM Interface
+      css: function() {
+          if (arguments.length === 1) {
+              return $(this.domNode()).css(arguments[0]);
+          } else {
+              $(this.domNode()).css(arguments[0], arguments[1]);
+          }
+      },
+
+      attr: function() {
+          if (arguments.length === 1) {
+              return $(this.domNode()).attr(arguments[0]);
+          } else {
+              $(this.domNode()).attr(arguments[0], arguments[1]);
+          }
+      },
+
       setEditable: function(editable) {
         $(node).attr('contenteditable', editable ? 'true' : 'false');
       },
--- test/jquery.jrichteqst.tests.js
+++ test/jquery.jrichteqst.tests.js
@@ -6,15 +6,26 @@ module("jRichTeQst");
 var testtext = $.fn.jRichTeQst($('#testtext')[0]);
     
 test("toString returns text contents", function() {
-  testtext.domNode().innerHTML = "Demo text";
-  equal( testtext.toString(), "Demo text", "Test element should contain 'Demo text'" );
+    testtext.domNode().innerHTML = "Demo text";
+    equal( testtext.toString(), "Demo text", "Test element should contain 'Demo text'" );
 });
 
 test("text becomes editable on click", function() {
-  testtext.setEditable(false);
-  $(testtext.domNode()).click();
-  ok( testtext.isEditable(), "text was not made editable on click");
+    testtext.setEditable(false);
+    $(testtext.domNode()).click();
+    ok( testtext.isEditable(), "text was not made editable on click");
 });
 
-}
+test("css sets css properties", function() {
+    testtext.css("color", "blue");
+    equal( testtext.css("color"), "rgb(0, 0, 255)", "color css attr. was not set" );
+});
 
+test("attr sets attribute", function() {
+    testtext.attr("name", "mytext");
+    equal( testtext.attr("name"), "mytext", "name attribute was not set on DOM node");
+
+});
+
+
+}
