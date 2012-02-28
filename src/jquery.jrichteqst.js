1d362b713c759d8bc238e5507d17581600a5e026
Fabian Bornhofen <fbornhofen@googlemail.com>
2012-02-28 01:08:02 -0800

    played with rangy. chrome still inserts div on pressing enter

--- src/jquery.jrichteqst.js
+++ src/jquery.jrichteqst.js
@@ -1,23 +1,23 @@
 (function( $ ){
 
   $.fn.jRichTeQst = function(node) {
-    var widget = {
+    var self = {
 
       // DOM Interface
       css: function() {
-          if (arguments.length === 1) {
-              return $(this.domNode()).css(arguments[0]);
-          } else {
-              $(this.domNode()).css(arguments[0], arguments[1]);
-          }
+        if (arguments.length === 1) {
+          return $(self.domNode()).css(arguments[0]);
+        } else {
+          $(self.domNode()).css(arguments[0], arguments[1]);
+        }
       },
 
       attr: function() {
-          if (arguments.length === 1) {
-              return $(this.domNode()).attr(arguments[0]);
-          } else {
-              $(this.domNode()).attr(arguments[0], arguments[1]);
-          }
+        if (arguments.length === 1) {
+          return $(self.domNode()).attr(arguments[0]);
+        } else {
+          $(this.domNode()).attr(arguments[0], arguments[1])
+        }
       },
 
       setEditable: function(editable) {
@@ -34,19 +34,54 @@
 
       domNode: function() {
         return node;
-      }
+      },
+
+      getSelectionRange: function() {
+	var sel = rangy.getSelection();
+        if (sel.containsNode(node, true)) {
+          return sel.getRangeAt(0);
+        } else {
+          return null;
+        }
+      },
+
+      insertAtCursor: function(aNode) {
+        var range = self.getSelectionRange();
+        if (!range) {
+            return;
+	}
+        range.insertNode(aNode);
+        self.placeCursorBehind(aNode);
+      },
+
+      placeCursorBehind: function(aNode) {
+        var range = rangy.createRange();
+        range.setStartAfter(aNode);
+        range.setEndAfter(aNode);
+      },
     };
 
     // event handling
 
     $(node).click(function() {
-       widget.setEditable(true);
+       self.setEditable(true);
     });
 
     $(node).mouseleave(function() {
-      widget.setEditable(false);
+      self.setEditable(false);
     });
 
-    return widget;
+    $(node).keypress(function(event) {   
+      if (event.which == 13 || event.which == 10) {
+        console.log('enter');
+        event.preventDefault();
+        event.stopPropagation();
+        self.insertAtCursor(document.createElement("br"));
+        return true;
+      }
+      return false;
+    });
+
+    return self;
   };
 })( jQuery );
